6a04e3225633d6d9702e73e925fa3970b942482c
dickymuliafiqri <santrialanwari@gmail.com>
2020-11-12 13:30:52 +0700

    Update boxstart.py

--- boxstart.py
+++ boxstart.py
@@ -4,9 +4,9 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(2,GPIO.OUT)  #LED Merah
-GPIO.setup(3,GPIO.OUT)  #LED Hijau
-GPIO.setup(4,GPIO.OUT)  #LED Biru
+GPIO.setup(3,GPIO.OUT)  #LED Merah
+GPIO.setup(5,GPIO.OUT)  #LED Hijau
+GPIO.setup(7,GPIO.OUT)  #LED Biru
 GPIO.setup(8,GPIO.IN)   #Sensor Aman
 GPIO.setup(10,GPIO.IN)  #Sensor Siaga I
 GPIO.setup(12,GPIO.IN)  #Sensor Siaga II
@@ -20,76 +20,78 @@ def tombol():
     print("Tombol telah ditekan")
 
 def netral():
-    GPIO.output(2,False)
     GPIO.output(3,False)
-    GPIO.output(4,False)
+    GPIO.output(5,False)
+    GPIO.output(7,False)
 
 def merah():
-    GPIO.output(2,True)
-    GPIO.output(3,False)
-    GPIO.output(4,False)
+    GPIO.output(3,True)
+    GPIO.output(5,False)
+    GPIO.output(7,False)
 
 def kuning():
-    GPIO.output(2,True)
     GPIO.output(3,True)
-    GPIO.output(4,False)
+    GPIO.output(5,True)
+    GPIO.output(7,False)
 
 def hijau():
-    GPIO.output(2,False)
-    GPIO.output(3,True)
-    GPIO.output(4,False)
-
-def mulai():
-    if (GPIO.input(8) == False):
-        print("Mulai")
-    else:
-        print("Cek siaga I")
-        siagaI()
+    GPIO.output(3,False)
+    GPIO.output(5,True)
+    GPIO.output(7,False)
 
 def siagaI():
-    if (GPIO.input(10) == False):
+    if (GPIO.input(8) == False):
         s1 = s1 + 1
         if s1 >= 20:
             while True:
                 try:
                     hijau()
-                    time.sleep(2)
+                    sleep(2)
                     netral()
-                    time.sleep(2)
+                    sleep(2)
                 except (GPIO.input(16) == True):
                     tombol()
         else:
-            print("Dari siaga I => mulai")
+            sleep(2)
     else:
-        print("Cek siaga II")
+        print("Cek Siaga II")
         siagaII()
 
 def siagaII():
-    if (GPIO.input(12) == False):
+    if (GPIO.input(10) == False):
         s2 = s2 + 1
         if s2 >= 10:
             while True:
                 try:
                     kuning()
-                    time.sleep(1)
+                    sleep(1)
                     netral()
-                    time.sleep(1)
+                    sleep(1)
                 except (GPIO.input(16) == True):
                     tombol()
         else:
-            print("Dari siaga II => mulai")
+            sleep(2)
     else:
+        print("Cek Bahaya")
+        bahaya()
+
+def bahaya():
+    if (GPIO.input(12) == False):
         while True:
             try:
                 merah()
-                time.sleep(0.5)
+                sleep(0.5)
+                netral()
+                sleep(0.5)
             except (GPIO.input(16) == True):
                 tombol()
-                GPIO.cleanup()
-
+    else:
+        print("Aman")
+        print("Mulai dari awal")
+        
 while True:
     try:
-        mulai()
+        siagaI()
         if s1 == 20:
             s1 = 0
             s2 = 0
@@ -99,6 +101,6 @@ while True:
         elif (GPIO.input(16) == True):
             s1 = 0
             s2 = 0
-        time.sleep(2)
+        sleep(2)
     except (KeyboardInterrupt,SystemExit):
         GPIO.cleanup()

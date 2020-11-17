d34ec0fe5cd5ccea00c18470c2bd5632519e6766
dickymuliafiqri <santrialanwari@gmail.com>
2020-11-17 10:49:52 +0700

    Rebase Script

--- aquabox.py
+++ aquabox.py
@@ -1,9 +1,7 @@
 import RPi.GPIO as GPIO
-import time
-import json
-import os
-import getpass
-from Scripts.aquanotif import notif,helper_count
+from Scripts.aquarefile import time, telepot, os, json, getpass, re, math, random, pandas
+from Scripts.aquarefile import nama, jam, tgl, thn, bln, full, cek, write_json, json_object
+from Scripts.aquanotif import notif
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -37,51 +35,16 @@ def hijau():
     GPIO.output(5,False)
     GPIO.output(7,True)
 
-tgl = time.strftime("%d %b", time.localtime())
-bln = time.strftime("%b %Y", time.localtime())
-thn = time.strftime("%Y", time.localtime())
-jam = time.strftime("%H:%M", time.localtime())
-
-nama = getpass.getuser()
-
 s1 = int()
 s2 = int()
 
-def write_json(data, filename=("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln))):
-    with open(filename, 'w') as jswrt:
-        json.dump(data, jswrt, indent = 4)
-
-tmprpt = {
-    "{}".format(tgl) : [
-        {
-            "Status" : "Mulai",
-            "Jam" : jam
-        }
-    ]
-}
-
-json_object = json.dumps(tmprpt, indent = 4)
-
 while True:
-    if os.path.exists("/home/{}/Documents/BoxDump.d".format(nama)) == False:
-        os.makedirs("/home/{}/Documents/BoxDump.d/{}".format(nama,thn))
-        with open("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln), "w") as outfile:
-            outfile.write(json_object)
-    else:
-        if os.path.exists("/home/{}/Documents/BoxDump.d/{}".format(nama,thn)) == False:
-            os.makedirs("/home/{}/Documents/BoxDump.d/{}".format(nama,thn))
-            with open("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln), "w") as outfile:
-                outfile.write(json_object)
-        else:
-            if os.path.exists("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln)) == False:
-                with open("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln), "w") as outfile:
-                    outfile.write(json_object)
+    cek()
     netral()
     if (GPIO.input(8) == False):
         s1 += 1
         if s1 >= 20:
             s1 = 0
-            helper_count()
             notif("Siaga I",jam)
             with open("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln)) as json_file:
                 data = json.load(json_file)
@@ -107,7 +70,6 @@ while True:
         s2 += 1
         if s2 >= 10:
             s2 = 0
-            helper_count()
             notif("Siaga II",jam)
             with open("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln)) as json_file:
                 data = json.load(json_file)
@@ -130,7 +92,6 @@ while True:
         else:
             print(("Status Siaga II Telah Terekam Sebanyak {} Kali").format(s2))
     elif (GPIO.input(12) == False):
-        helper_count()
         notif("Bahaya",jam)
         with open("/home/{}/Documents/BoxDump.d/{}/{}.json".format(nama,thn,bln)) as json_file:
             data = json.load(json_file)

25e8bdfbef80f95f5b70501203bd5e54a488a5e6
Dicky Mulia Fiqri <dickymuliafiqri@localhost.localdomain>
2020-11-19 19:43:49 +0700

    Update aquasetup.py

--- aquasetup.py
+++ aquasetup.py
@@ -2,38 +2,6 @@ import mysql.connector, json, time
 
 json_setup = json.loads(open("setup.json").read())
 
-def create_db():
-    db = mysql.connector.connect(
-    host=json_setup["host"],
-    user=json_setup["user"],
-    passwd=json_setup["passwd"]
-    )
-
-    if db.is_connected():
-        print("Koneksi Ke DataBase Berhasil !!!")
-        print("Membuat DataBase Baru")
-        time.sleep(2)
-    cursor = db.cursor()
-    cursor.execute("CREATE DATABASE TheBox")
-    print("Database TheBox berhasil dibuat")
-
-def tabel_db():
-    db = mysql.connector.connect(
-    host=json_setup["host"],
-    user=json_setup["user"],
-    passwd=json_setup["passwd"],
-    database=json_setup["database"]
-    )
-    cursor = db.cursor()
-    sql = """CREATE TABLE BoxDump (
-        tanggal DATE(),
-        status CHAR(9),
-        jam TIME()
-    )
-    """
-    cursor.execute(sql)
-    print("Tabel BoxDump Telah Berhasil Dibuat")
-
 def insert_db(status):
     jam = time.strftime("%H:%M:%S", time.localtime())
     full = time.strftime("%Y/%m/%d", time.localtime())
@@ -42,6 +10,7 @@ def insert_db(status):
     user=json_setup["user"],
     passwd=json_setup["passwd"]
     )
+
     cursor = db.cursor()
     sql = "INSERT INTO BoxDump (tanggal, status, jam) VALUES (%s, %s, %s)"
     val = (full, status, jam)
@@ -50,6 +19,40 @@ def insert_db(status):
     print("Status {} Pukul {} Telah Berhasil Ditambahkan".format(status, jam))
 
 if __name__ == "__main__":
+    def create_db():
+        db = mysql.connector.connect(
+        host=json_setup["host"],
+        user=json_setup["user"],
+        passwd=json_setup["passwd"]
+        )
+
+        if db.is_connected():
+            print("Koneksi Ke DataBase Berhasil !!!")
+            print("Membuat DataBase Baru")
+            time.sleep(2)
+        cursor = db.cursor()
+        cursor.execute("CREATE DATABASE TheBox")
+        print("Database TheBox berhasil dibuat")
+
+    def tabel_db():
+        print("Membuat Tabel")
+        db = mysql.connector.connect(
+        host=json_setup["host"],
+        user=json_setup["user"],
+        passwd=json_setup["passwd"],
+        database=json_setup["database"]
+        )
+
+        cursor = db.cursor()
+        sql = """CREATE TABLE BoxDump (
+            tanggal DATE(),
+            status CHAR(9),
+            jam TIME()
+        )
+        """
+        cursor.execute(sql)
+        print("Tabel BoxDump Telah Berhasil Dibuat")
+
     create_db()
     tabel_db()
     print("Jalankan aquastart.py untuk memulai monitoring")

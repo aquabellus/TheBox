9094b31814f8841fce34bfffb05c32cffd6b2588
Aqua Bellus <aquabellus@localhost.localdomain>
2020-11-25 12:44:55 +0700

    Fix bugs

--- src/aquabot.sh
+++ src/aquabot.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
 #   Created on 22th Nov 2020
 #   08:11 AM
 #   @aquabellus
 #
 
-declare -r input=$(eval echo $HOME)/Documents/The-Box/helper/aquabot.pid
+declare -r dir=$(eval echo $HOME)/Documents/The-Box
 
 while true; do
-    if ps -p $(cat $input) | grep -o $(cat $input); then
+    cd $dir
+    if ps -p $(cat helper/aquabot.pid) | grep -o $(cat helper/aquabot.pid); then
         sleep 1
     else
-        python3 $(eval echo $HOME)/Documents/The-Box/aquabot.py
+        python3 aquabot.py
     fi
     sleep 5
+    clear
 done

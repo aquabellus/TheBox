db60e9bbc06f5f7e08d2eeac40d037613eb8318c
Dicky Mulia Fiqri <dickymuliafiqri@localhost.localdomain>
2020-11-30 20:37:37 +0700

    Konfigurasi penambahan sensor

--- aquastart.py
+++ aquastart.py
@@ -9,13 +9,15 @@ logging.basicConfig(filename='log/aqualog.log', filemode='a', format='%(asctime)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(3,GPIO.OUT)  #LED Merah
-GPIO.setup(5,GPIO.OUT)  #LED Hijau
-GPIO.setup(7,GPIO.OUT)  #LED Biru
-GPIO.setup(8,GPIO.IN)   #Sensor Siaga I
-GPIO.setup(10,GPIO.IN)  #Sensor Siaga II
-GPIO.setup(12,GPIO.IN)  #Sensor Bahaya
-GPIO.setup(16,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) #Tombol
+GPIO.setup(3, GPIO.OUT)  #LED Merah
+GPIO.setup(5, GPIO.OUT)  #LED Hijau
+GPIO.setup(7, GPIO.OUT)  #LED Biru
+GPIO.setup(8, GPIO.IN)   #Sensor Siaga I
+GPIO.setup(10, GPIO.IN)  #Sensor Siaga II
+GPIO.setup(12, GPIO.IN)  #Sensor Siaga III
+GPIO.setup(16, GPIO.IN) #Sensor Siaga IV
+GPIO.input(18, GPIO.IN) #Sensor Bahaya
+GPIO.input(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Tombol
 
 def netral():
     GPIO.output(3,True)
@@ -43,7 +45,7 @@ tmprpt = {
         {
             "Timestamp" : datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
             "Status" : "Mulai",
-            "Tinggi" : "0M",
+            "Tinggi" : "0 cm",
             "Jam" : datetime.datetime.now().strftime("%H:%M:%S")
         }
     ]
@@ -84,12 +86,20 @@ def status():
     if (GPIO.input(8) == False):
         status = str("Siaga I")
         if (GPIO.input(10) == False):
-            status = str("Siaga I/II")
+            status = str("Siaga I|II")
     elif (GPIO.input(10) == False):
         status = str("Siaga II")
         if (GPIO.input(12) == False):
-            status = str("Siaga II/Bahaya")
+            status = str("Siaga II|III")
     elif (GPIO.input(12) == False):
+        status = str("Siaga III")
+        if (GPIO.input(16) == False):
+            status = str("Siaga III|IV")
+    elif (GPIO.input(16) == False):
+        status = str("Siaga IV")
+        if (GPIO.input(18) == False):
+            status = str("Siaga IV|Bahaya")
+    elif (GPIO.input(18) == False):
         status = str("Bahaya")
     return(status)
 
@@ -99,12 +109,20 @@ def tinggi():
     if (GPIO.input(8) == False):
         tinggi = str(json_setup["SI"])
         if (GPIO.input(10) == False):
-            tinggi = str(json_setup["SI/II"])
+            tinggi = str(json_setup["SI|II"])
     elif (GPIO.input(10) == False):
         tinggi = str(json_setup["SII"])
         if (GPIO.input(12) == False):
-            tinggi = str(json_setup["SII/B"])
+            tinggi = str(json_setup["SII|SIII"])
     elif (GPIO.input(12) == False):
+        tinggi = str(json_setup["SIII"])
+        if (GPIO.input(16) == False):
+            tinggi = str(json_setup["SIII|IV"])
+    elif (GPIO.input(16) == False):
+        tinggi = str(json_setup["SIV"])
+        if (GPIO.input(18) == False):
+            tinggi = str(json_setup["SIV|B"])
+    elif (GPIO.input(18) == False):
         tinggi = str(json_setup["B"])
     return(tinggi)
 
@@ -113,15 +131,27 @@ def insert_server(timestamp, tanggal, waktu, ketinggian, status):
     tt = re.sub(r"[/]", "-", tanggal)
     wk = re.sub(r"[:]", "%3A", waktu)
     sT = re.sub(r"[ ]", "+", status)
-    st = re.sub(r"[/]", "%2F", sT)
-    tg = re.sub(r"[/]", "%2F", ketinggian)
-    request.urlopen("http://10.30.1.247/proses.php?Timestamp={}&Tanggal={}&Waktu={}&Ketinggian={}+M&Status={}".format(timestamp, tt, wk, tg, st))
+    st = re.sub(r"[|]", "%7C", sT)
+    tg = re.sub(r"[|]", "%7C", ketinggian)
+    request.urlopen("http://10.30.1.247/proses.php?Timestamp={}&Tanggal={}&Waktu={}&Ketinggian={}+cm&Status={}".format(timestamp, tt, wk, tg, st))
     print("Data telah terkirim ke database")
     sleep(1)
+
+def bundle():
+    notif(str_status)
+    insert_db(timestamp, str_status, str_tinggi, jam)
+    insert_server(timestamp, full, jam, str_tinggi, str_status)
+    with open("/home/{}/Documents/BoxDump.d/BoxDump.json".format(nama)) as json_file:
+        data = json.load(json_file)
+        temp = data["{}".format(full)]
+        temp.append(report)
+    write_json(data)
 
 s1 = int()
 s2 = int()
 s3 = int()
+s4 = int()
+s5 = int()
 
 if __name__ == "__main__":
     from aquabot import notif, alert, pressed
@@ -145,80 +175,56 @@ if __name__ == "__main__":
             if (GPIO.input(8) == False):
                 if (GPIO.input(10) == False):
                     s2 += 1
-                    if s2 == 10:
-                        notif(str_status)
-                        insert_db(timestamp, str_status, str_tinggi, jam)
-                        insert_server(timestamp, full, jam, str_tinggi, str_status)
-                        with open("/home/{}/Documents/BoxDump.d/BoxDump.json".format(nama)) as json_file:
-                            data = json.load(json_file)
-                            temp = data["{}".format(full)]
-                            temp.append(report)
-                        write_json(data)
+                    if s2 == 20:
+                        bundle()
                         s2 = 0
                 s1 += 1
                 if s1 == 20:
-                    notif(str_status)
-                    insert_db(timestamp, str_status, str_tinggi, jam)
-                    insert_server(timestamp, full, jam, str_tinggi, str_status)
-                    with open("/home/{}/Documents/BoxDump.d/BoxDump.json".format(nama)) as json_file:
-                        data = json.load(json_file)
-                        temp = data["{}".format(full)]
-                        temp.append(report)
-                    write_json(data)
+                    bundle()
                     s1 = 0
             elif (GPIO.input(10) == False):
                 if (GPIO.input(12) == False):
                     s3 += 1
-                    if s3 == 5:
-                        notif(str_status)
-                        insert_db(timestamp, str_status, str_tinggi, jam)
-                        insert_server(timestamp, full, jam, str_tinggi, str_status)
-                        with open("/home/{}/Documents/BoxDump.d/BoxDump.json".format(nama)) as json_file:
-                            data = json.load(json_file)
-                            temp = data["{}".format(full)]
-                            temp.append(report)
-                        write_json(data)
+                    if s3 == 10:
+                        bundle()
                         s3 = 0
                 s2 += 1
-                if s2 == 10:
-                    notif(str_status)
-                    insert_db(timestamp, str_status, str_tinggi, jam)
-                    insert_server(timestamp, full, jam, str_tinggi, str_status)
-                    with open("/home/{}/Documents/BoxDump.d/BoxDump.json".format(nama)) as json_file:
-                        data = json.load(json_file)
-                        temp = data["{}".format(full)]
-                        temp.append(report)
-                    write_json(data)
+                if s2 == 20:
+                    bundle()
                     s2 = 0
-            elif (GPIO.input(12) == False):
-                if (GPIO.input(10) == True):
-                    notif(str_status)
-                    insert_db(timestamp, str_status, str_tinggi, jam)
-                    insert_server(timestamp, full, jam, str_tinggi, str_status)
-                    with open("/home/{}/Documents/BoxDump.d/BoxDump.json".format(nama)) as json_file:
-                        data = json.load(json_file)
-                        temp = data["{}".format(full)]
-                        temp.append(report)
-                    write_json(data)
-                    s2 = 0
-                    while (GPIO.input(16) == False):
+            elif (GPIO.input(16) == False):
+                if (GPIO.input(18) == False):
+                    s5 += 1
+                    if s5 == 5:
+                        bundle()
+                        s5 = 0
+                s4 += 1
+                if s4 == 10:
+                    bundle()
+                    s4 = 0
+            elif (GPIO.input(18) == False):
+                if (GPIO.input(16) == True):
+                    bundle()
+                    while (GPIO.input(22) == False):
                         netral()
                         sleep(1)
                         merah()
                         sleep(1)
                         alert()
                         sleep(1)
-                        if (GPIO.input(16) == True):
+                        if (GPIO.input(22) == True):
                             print("Tombol telah ditekan")
                             pressed()
                             break
-
+                    s5 = 0
             if (re.compile(r"00:00:0\d").search(jam)):
                 for ulang in range(2):
                     cek()
                 s1 = 0
                 s2 = 0
                 s3 = 0
+                s4 = 0
+                s5 = 0
 
             print("#########################")
             print("")
@@ -230,7 +236,9 @@ if __name__ == "__main__":
             print("Hasil :")
             print("Siaga I : {}".format(s1))
             print("Siaga II : {}".format(s2))
-            print("Bahaya : {}".format(s3))
+            print("Siaga III : {}".format(s3))
+            print("Siaga IV : {}".format(s4))
+            print("Bahaya : {}".format(s5))
             sync_db(nama)
         except:
             logging.warning("This will get logged to a file")
